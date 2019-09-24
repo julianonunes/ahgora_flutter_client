63b492966d0df8ed23c5a3e3be20842e6a32cbd6
Juliano Nunes Silva Oliveira <julianonunes@live.com>
2019-09-24 10:10:56 -0300

    Implemented the local saving and API post

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,45 @@
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
 class User {
-  String token;
-  String login;
+  final tokenKey = 'token_key';
+  final loginKey = 'login_key';
+  final passwordKey = 'password_key';
+
+  String identity;
+  String account;
   String password;
 
-  save() {
+  read() async {
+    final prefs = await SharedPreferences.getInstance();
+    identity = prefs.getString(tokenKey);
+    account = prefs.getString(loginKey);
+    password = prefs.getString(passwordKey);
+  }
+
+  Future<http.Response> register() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(tokenKey, identity);
+    prefs.setString(loginKey, account);
+    prefs.setString(passwordKey, password);
     
+    final response = await http.post('https://www.ahgora.com.br/batidaonline/verifyIdentification',
+      headers: {
+          HttpHeaders.contentTypeHeader: 'application/json',
+          HttpHeaders.authorizationHeader : ''
+        },
+        body: json.encode({
+          identity: this.identity,
+          account: this.account,
+          password: this.password,
+          'origin': 'pw2',
+          'key': ''
+        })
+      );
+
+    return response;
   }
 }
